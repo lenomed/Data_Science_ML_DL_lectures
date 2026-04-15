87c321fb1f706f5d548001f9b0f63651869a5964
lenomed <lenomedmega@gmail.com>
2026-04-15 14:49:42 +0100

    merging joining and concatenating

---
+++ Pandas_lib/merging joining and concatenating.py
@@ -0,0 +1,51 @@
+import numpy as np
+import pandas as pd
+
+df1 = pd.DataFrame({'A': ['A0', 'A1', 'A2', 'A3'],
+                        'B': ['B0', 'B1', 'B2', 'B3'],
+                        'C': ['C0', 'C1', 'C2', 'C3'],
+                        'D': ['D0', 'D1', 'D2', 'D3']},
+                        index=[0, 1, 2, 3])
+df2 = pd.DataFrame({'A': ['A4', 'A5', 'A6', 'A7'],
+                        'B': ['B4', 'B5', 'B6', 'B7'],
+                        'C': ['C4', 'C5', 'C6', 'C7'],
+                        'D': ['D4', 'D5', 'D6', 'D7']},
+                         index=[4, 5, 6, 7])
+df3 = pd.DataFrame({'A': ['A8', 'A9', 'A10', 'A11'],
+                        'B': ['B8', 'B9', 'B10', 'B11'],
+                        'C': ['C8', 'C9', 'C10', 'C11'],
+                        'D': ['D8', 'D9', 'D10', 'D11']},
+                        index=[8, 9, 10, 11])
+
+conc_data = pd.concat([df1, df2, df3])# concatenation basically glues dataframes
+print(conc_data)
+print("_"*50)
+
+
+left1 = pd.DataFrame({'key1': ['K0', 'K0', 'K1', 'K2'],
+                     'key2': ['K0', 'K1', 'K0', 'K1'],
+                     'A': ['A0', 'A1', 'A2', 'A3'],
+                     'B': ['B0', 'B1', 'B2', 'B3']})
+
+right2 = pd.DataFrame({'key1': ['K0', 'K1', 'K1', 'K2'],
+                      'key2': ['K0', 'K0', 'K0', 'K0'],
+                      'C': ['C0', 'C1', 'C2', 'C3'],
+                      'D': ['D0', 'D1', 'D2', 'D3']})
+merge_data = left1.merge(right2, on = ["key1", "key2"], how = "outer")##  inner abstracts columns with same key to one key while outer does otherwise
+merge_data2 = pd.merge(left1, right2, on = ["key1", "key2"], how = "outer")
+print(merge_data)
+print("_"*50)
+print(merge_data2)
+
+print("_"*50)
+#Joining is a convenient method for combining the columns of two potentially differently-indexed DataFrames into a single result DataFrame.
+left_j = pd.DataFrame({'A': ['A0', 'A1', 'A2'],
+                     'B': ['B0', 'B1', 'B2']},
+                      index=['K0', 'K1', 'K2'])
+
+right_j = pd.DataFrame({'C': ['C0', 'C2', 'C3'],
+                    'D': ['D0', 'D2', 'D3']},
+                      index=['K0', 'K2', 'K3'])
+
+join_data = left_j.join(right_j, how="inner")
+print(join_data)
