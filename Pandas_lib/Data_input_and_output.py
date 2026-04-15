7db4d625f329e037376cbb0316ac9ed8c7b88f8d
lenomed <lenomedmega@gmail.com>
2026-04-15 17:50:16 +0100

    data input and output

--- Pandas_lib/Data_input_and_output.py
+++ Pandas_lib/Data_input_and_output.py
@@ -8,29 +8,10 @@ print(write_to_csv)
 print(pd.read_csv('new_csv'))
 print("_"*50)
 
-import pandas as pd
+pd.read_excel('Excel_Sample.xlsx',sheetname='Sheet1')
+write_to_x = df.to_excel('Excel_Sample.xlsx',sheet_name='Sheet1')
+print(write_to_x)
 
-# 1. Read the original Excel file
-df2 = pd.read_excel('Excel_Sample.xlsx', sheet_name='Sheet1')
 
-print("Original DataFrame:")
-print(df2)
-print("_" * 50)
-
-# 2. Write to new Excel file (correct way)
-df2.to_excel('new_excel.xlsx',
-             sheet_name='sheet1',   # Use consistent sheet name
-             index=False)
-
-print("Data successfully written to 'new_excel.xlsx' (sheet: new_sheet)")
-print("_" * 50)
-
-# 3. Read back from the new file (use the correct sheet name)
-readx = pd.read_excel('new_excel.xlsx', sheet_name='sheet1')
-
-print("Data read back from new file:")
-print(readx)
-print("_" * 50)
-
-# Optional: Check if they are the same
-print("Are both DataFrames equal?", df2.equals(readx))
+df3 = pd.read_html('http://www.fdic.gov/bank/individual/failed/banklist.html')
+print(df3[0])
