5e60bca162688c785f83fc2fb371413d58098271
lenomed <lenomedmega@gmail.com>
2026-04-21 15:28:28 +0100

    matrix plots final

---
+++ Seaborn/grid.py
@@ -0,0 +1,4 @@
+import matplotlib as plt
+import seaborn as sns
+
+sns.load_dataset('flights')
--- Seaborn/matrix_plots.py
+++ Seaborn/matrix_plots.py
@@ -1,3 +1,4 @@
+from operator import index
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -14,7 +15,12 @@ heat_flight = flights.select_dtypes(include='number').corr()
 plt.show()
 
 flights_matrix = flights.pivot_table(index='month', columns='year', values='passengers')
-sns.heatmap(flights_matrix, cmap='coolwarm')
+sns.heatmap(flights_matrix, cmap='coolwarm', linecolor='#ffff', linewidths='1')# maga in cmap can be used to display data from warm to cool, just like anti clock wise but not realy, it gives you the opposite of the supposed display method
 plt.tight_layout()
 plt.show()
 print(flights_matrix)
+
+#it uses heirachical clustering to display the clustered values of the data
+
+sns.clustermap(flights_matrix, standard_scale=1)
+plt.show()
