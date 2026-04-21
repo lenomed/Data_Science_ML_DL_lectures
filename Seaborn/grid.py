6e381bd16daf26720e58cfbc3862254151691a79
lenomed <lenomedmega@gmail.com>
2026-04-21 19:36:41 +0100

    chore

--- Seaborn/grid.py
+++ Seaborn/grid.py
@@ -1,4 +1,16 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import seaborn as sns
 
-sns.load_dataset('flights')
+iris = sns.load_dataset('iris')
+g = sns.PairGrid(iris)
+g.map_diag(sns.histplot)
+g.map_upper(sns.kdeplot)
+g.map_lower(plt.scatter)
+g.map(plt.scatter)
+plt.show()
+
+tips = sns.load_dataset('tips')
+f_grid = sns.FacetGrid(data=tips, col='time', row='smoker')
+f_grid.map(sns.histplot, 'total_bill')
+plt.show()
+
