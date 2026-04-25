bb7784e96b8fbf04269127789ff31df18973f1dc
lenomed <lenomedmega@gmail.com>
2026-04-25 18:33:39 +0100

    chore

--- geographical_plot/choropleth_maps.py
+++ geographical_plot/choropleth_maps.py
@@ -15,5 +15,18 @@ layout = dict(geo={'scope': 'usa'})
 choromap = go.Figure(data = [data], layout = layout)
 #choromap.show()
 
-df = pd.read_csv('2011_US_AGRI_Exports.csv')
-print(df.head())
+df = pd.read_csv(r'C:\Users\megam\source\repos\Data_Science_ML_DL_lectures\geographical_plot\2011_US_AGRI_Exports.csv')
+
+data2 = dict(type='choropleth',
+             locations=df['code'],
+             locationmode='USA-states',        # was 'USA-state' (missing s)
+             colorscale='portland',
+             text=df['text'],
+             z=df['total exports'],             # z should be a numeric column
+             colorbar=dict(title='2011 Agricultural Exports in USA')  # lowercase colorbar, dict() not {}
+             )
+
+layout = dict(geo={'scope': 'usa'})
+
+choromap2 = go.Figure(dict (title='Agro Us', data=[go.Choropleth(data2)]), geo = dict(layout=layout))
+choromap2.show()
