67ab08cd873aa96c7466cd3c1201e0cabf5b2ebb
lenomed <lenomedmega@gmail.com>
2026-05-02 19:59:49 +0100

    chore

---
+++ Machine_Learning/logistics_regression_theory.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+train = pd.read_csv(r'C:\Users\megam\source\repos\Data_Science_ML_DL_lectures\Machine_Learning\titanic_train.csv')
+print(train.head())
