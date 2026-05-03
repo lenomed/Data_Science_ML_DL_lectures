4a7bb61cc0532e00ba452da3d2d19afb193ca7d7
lenomed <lenomedmega@gmail.com>
2026-05-04 00:54:28 +0100

    chore

--- Machine_Learning/logistics_regression_theory.py
+++ Machine_Learning/logistics_regression_theory.py
@@ -5,3 +5,13 @@ import seaborn as sns
 
 train = pd.read_csv(r'C:\Users\megam\source\repos\Data_Science_ML_DL_lectures\Machine_Learning\titanic_train.csv')
 print(train.head())
+
+print(train.isnull())
+
+sns.heatmap(train.isnull())
+plt.show()
+sns.countplot(x='Survived', data=train, hue='Sex')
+plt.show()
+
+# dealing with missing data
+
